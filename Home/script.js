f5dbe46472084bc12e2aedfef53fe363163e468f
LoPhine <loophinesj@gmail.com>
2024-06-11 17:07:48 -0300

    finishing home page

--- Home/script.js
+++ Home/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const flowerDivs = document.querySelectorAll('.flower');
     flowerDivs.forEach(div => {
         const img = document.createElement('img');
-        img.src = './MoodBoard/Flower.png'; 
+        img.src = '../Images/Flower.png'; 
         img.alt = 'Flor';
         img.classList.add('flower-image'); 
         div.appendChild(img);
